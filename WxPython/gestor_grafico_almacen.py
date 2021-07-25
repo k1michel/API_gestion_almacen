879b2b555c75ea78bc518d0d95704722738fcbd5
Miguel Ángel Álvarez <michel_civic@hotmail.com>
2021-07-25 15:51:33 +0200

    Introduzco lista desplegable con categorias y busqueda general: las guarda en un dict. PERO interfaz grafica desorganizada.

--- WxPython/gestor_grafico_almacen.py
+++ WxPython/gestor_grafico_almacen.py
@@ -11,17 +11,18 @@ class interfaz(wx.Frame):
     def InitUI(self):
 
         self.pnl = wx.Panel(self)
-
+        self.categoria_sel = str
+        self.busqueda_enter = str
         ## VENTANA ##
         self.SetSize((1080, 720))       
         self.SetTitle('Gestion Almacen')  
         self.SetBackgroundColour((0, 176, 246))
         self.Centre()
 
-        self.fgs = wx.FlexGridSizer(6, 2, 1,1)
+        self.fgs = wx.FlexGridSizer(8, 3, 1,1)
         self.sizer = wx.BoxSizer()
  
-        ## Huecos
+        ## Huecos matriz grafica
         self.hueco1= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.hueco1.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
         self.hueco2= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
@@ -55,14 +56,21 @@ class interfaz(wx.Frame):
         self.titulo_bucar= wx.StaticText(self.pnl, label= 'Buscar Material', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.titulo.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
 
-        self.ctrl_buscar= wx.TextCtrl(self.pnl)
+        self.ctrl_buscar= wx.TextCtrl(self.pnl, style= wx.TE_PROCESS_ENTER)
+        self.ctrl_buscar.Bind(wx.EVT_TEXT_ENTER,self.OnEnterPressed) 
 
         ## Resultado busqueda
         self.txt_resultado= wx.StaticText(self.pnl, label= 'Resultado Busqueda', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_resultado.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
         
         self.ctrl_resultado = wx.TextCtrl(self.pnl, size= (300,500),style = wx.TE_MULTILINE)
-    
+         
+        # Datos para los ComboBox
+        self.categoria =  u"electricidad|neumatica".split("|")
+        self.cbbox_categoria = wx.ComboBox(self,-1,choices= self.categoria,size=(120,30))
+        self.Bind(wx.EVT_COMBOBOX, self.OnSelect)
+
+        self.ctrl_categoria = wx.TextCtrl(self.pnl, size= (300,500),style = wx.TE_MULTILINE)
 
         self.fgs.AddMany([(self.titulo,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco1,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.closeButton,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco2,1,wx.ALIGN_CENTRE_HORIZONTAL),
         (self.hueco3,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.titulo_bucar,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco4,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.ctrl_buscar,1,wx.ALIGN_CENTRE_HORIZONTAL),
@@ -74,15 +82,28 @@ class interfaz(wx.Frame):
         self.pnl.SetSizerAndFit(self.sizer)
         
 
-    def OnClicked(self,e):                             #creo funcion de clickado
-        print("El boton de CERRAR ha sido presionado")     
-        self.OnClose(True)                                     
+
+                                     
         
     def OnClose(self,e):                                #defino la funcion de cerrar ventana
         self.Close(True)
 
-
-
+    def OnSelect(self,event):
+        self.categoria_sel = self.cbbox_categoria.GetValue()
+        self.enviar(True)
+    def OnEnterPressed(self,event):
+        self.busqueda_enter = self.ctrl_buscar.GetValue()
+        self.enviar(True)
+    def OnClicked(self,e):                             #creo funcion de clickado
+        print("El boton de CERRAR ha sido presionado")     
+        print(self.envio)
+        self.OnClose(True)
+    
+    def enviar(self,e):
+        self.envio = dict(
+            cat= self.categoria_sel,
+            busc= self.busqueda_enter
+            )
 
 def main():
 
