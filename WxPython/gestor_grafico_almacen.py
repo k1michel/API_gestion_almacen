db0e1b4ef7987e80533504933389f526edd33b06
Miguel Ángel Álvarez <michel_civic@hotmail.com>
2021-07-26 17:59:01 +0200

    Cambio interfaz ahora con GridBagSizer. Sin comprobar resultado...

--- WxPython/gestor_grafico_almacen.py
+++ WxPython/gestor_grafico_almacen.py
@@ -19,84 +19,10 @@ class interfaz(wx.Frame):
         self.SetBackgroundColour((0, 176, 246))
         self.Centre()
 
-        self.fgs = wx.FlexGridSizer(12, 5, 1,1)
+        self.sizer = wx.GridBagSizer(12, 5)
         
  
-        ## Huecos matriz grafica
-        self.hueco1= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco1.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco2= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco2.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco3= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco3.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco4= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco4.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco5= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco5.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco6= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco6.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco7= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco7.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco8= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco8.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco9= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco9.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco10= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco10.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco11= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco11.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco12= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco12.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco13= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco13.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco14= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco14.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco15= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco15.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco16= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco16.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco17= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco17.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco18= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco18.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco19= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco19.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco20= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco20.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco21= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco21.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco22= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco22.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco23= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco23.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco24= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco24.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco25= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco25.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco26= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco26.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco27= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco27.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco28= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco28.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco29= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco29.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco30= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco30.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco31= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco31.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco32= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco32.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco33= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco33.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco34= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco34.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco35= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco35.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco36= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco36.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.hueco37= wx.StaticText(self.pnl, label= ' ', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
-        self.hueco37.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+
         
         ## Cuadro titulo
         self.cuadro_titulo= wx.StaticBox(self.pnl, label="", pos=(200,2),size=(400,85), style= wx.ALIGN_CENTRE_HORIZONTAL)
@@ -108,100 +34,110 @@ class interfaz(wx.Frame):
         self.titulo= wx.StaticText(self.pnl, label= 'GESTION ALMACEN', pos= (600,80), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.titulo.SetFont(self.fuente_titulo)
         self.titulo.SetForegroundColour(wx.Colour(255,255,255))
-
+        self.sizer.Add(self.titulo, pos=(0, 2), flag=wx.TOP, border=5)
         ### Boton Cerrar ###
         self.closeButton = wx.Button(self.pnl, label='Cerrar', pos= (540, 5))   
         self.closeButton.Bind(wx.EVT_BUTTON, self.OnClickedCerrar)
         self.closeButton.SetForegroundColour(wx.Colour(255,255,255))   
         self.closeButton.SetBackgroundColour(wx.Colour(0,0,0)) 
+        self.sizer.Add(self.closeButton, pos=(1, 2), flag=wx.TOP, border=5)
 
         ## Listado 
         self.txt_listado= wx.StaticText(self.pnl, label= 'Listado', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_listado.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_listado, pos=(3, 0), flag=wx.TOP, border=5)
 
         self.ctrl_buscar= wx.TextCtrl(self.pnl, style= wx.TE_PROCESS_ENTER)
         self.ctrl_buscar.Bind(wx.EVT_TEXT_ENTER,self.OnEnterPressedBuscar) 
-        
+        self.sizer.Add(self.ctrl_buscar, pos=(5, 0), flag=wx.TOP, border=5)
         
         self.categoria =  u"electricidad|neumatica".split("|")
         self.cbbox_categoria = wx.ComboBox(self,-1,choices= self.categoria,size=(120,30))
         self.Bind(wx.EVT_COMBOBOX, self.OnSelect)
         #self.ctrl_categoria = wx.TextCtrl(self.pnl, size= (300,500),style = wx.TE_MULTILINE)
+        self.sizer.Add(self.cbbox_categoria, pos=(4, 0), flag=wx.TOP, border=5)
 
         self.ctrl_resultado = wx.TextCtrl(self.pnl, size= (200,600),style = wx.TE_MULTILINE)
-         
+        self.sizer.Add(self.ctrl_resultado, pos=(6, 0), flag=wx.TOP, border=5)
         
 
         ## Referenciado
         self.txt_referenciado= wx.StaticText(self.pnl, label= 'Referenciado', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_referenciado.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_referenciado, pos=(3, 3), flag=wx.TOP, border=5)
 
         self.ctrl_buscar_codigo= wx.TextCtrl(self.pnl, style= wx.TE_PROCESS_ENTER)
         self.ctrl_buscar_codigo.Bind(wx.EVT_TEXT_ENTER,self.OnEnterPressedCodigo)
         self.ctrl_buscar_codigo.SetValue('Codigo') 
+        self.sizer.Add(self.ctrl_buscar_codigo, pos=(4, 3), flag=wx.TOP, border=5)
 
         self.txt_codigo= wx.StaticText(self.pnl, label= 'Codigo', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_codigo.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_codigo, pos=(5, 2), flag=wx.TOP, border=5)
+        
         self.ctrl_codigo= wx.TextCtrl(self.pnl)
+        self.sizer.Add(self.ctrl_codigo, pos=(6, 2), flag=wx.TOP, border=5)
 
         self.txt_categoria= wx.StaticText(self.pnl, label= 'Categoria', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_categoria.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_categoria, pos=(5, 3), flag=wx.TOP, border=5)
+        
         self.ctrl_categoria= wx.TextCtrl(self.pnl)
+        self.sizer.Add(self.ctrl_categoria, pos=(6, 3), flag=wx.TOP, border=5)
 
         self.txt_modelo= wx.StaticText(self.pnl, label= 'Modelo', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_modelo.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_modelo, pos=(5, 4), flag=wx.TOP, border=5)
+
         self.ctrl_modelo= wx.TextCtrl(self.pnl)
+        self.sizer.Add(self.ctrl_modelo, pos=(6, 4), flag=wx.TOP, border=5)
 
         self.txt_stock= wx.StaticText(self.pnl, label= 'Stock', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_stock.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_stock, pos=(7, 2), flag=wx.TOP, border=5)
+
         self.ctrl_stock= wx.TextCtrl(self.pnl)
+        self.sizer.Add(self.ctrl_stock, pos=(8, 2), flag=wx.TOP, border=5)
 
         self.txt_fecha= wx.StaticText(self.pnl, label= 'Fecha', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_fecha.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        self.ctrl_fecha= wx.TextCtrl(self.pnl)
+        self.sizer.Add(self.txt_fecha, pos=(7, 3), flag=wx.TOP, border=5)
 
+        self.ctrl_fecha= wx.TextCtrl(self.pnl)
+        self.sizer.Add(self.ctrl_fecha, pos=(8, 3), flag=wx.TOP, border=5)
         #Modificar y refrescar
         self.modificarButton = wx.Button(self.pnl, label='Modificar', pos= (540, 5))   
         self.modificarButton.Bind(wx.EVT_BUTTON, self.OnClickedModificar) 
         self.modificarButton.SetBackgroundColour(wx.Colour(0,0,0))
         self.modificarButton.SetForegroundColour(wx.Colour(255,255,255))
+        self.sizer.Add(self.modificarButton, pos=(9, 3), flag=wx.TOP, border=5)
 
         self.refrescarButton = wx.Button(self.pnl, label='Refrescar', pos= (540, 5))   
         self.refrescarButton.Bind(wx.EVT_BUTTON, self.OnClickedRefrescar) 
         self.refrescarButton.SetBackgroundColour(wx.Colour(0,0,0))
         self.refrescarButton.SetForegroundColour(wx.Colour(255,255,255))
+        self.sizer.Add(self.refrescarButton, pos=(10, 3), flag=wx.TOP, border=5)
 
         #Logo
         self.txt_logo1= wx.StaticText(self.pnl, label= 'Logo1', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_logo1.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_logo1, pos=(0, 1), flag=wx.TOP, border=5)
 
         self.txt_logo2= wx.StaticText(self.pnl, label= 'Logo2', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_logo2.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_logo2, pos=(0, 3), flag=wx.TOP, border=5)
 
         ## Imagen
         self.txt_imagen= wx.StaticText(self.pnl, label= 'Imagen', pos= (540,120), style= wx.ALIGN_CENTRE_HORIZONTAL)
         self.txt_imagen.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        self.sizer.Add(self.txt_imagen, pos=(11, 3), flag=wx.TOP, border=5)
 
+        self.sizer.AddGrowableCol(0)
+        sizer.AddGrowableRow(11)
 
-        self.fgs.AddMany([(self.hueco1,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_logo1,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.titulo,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_logo2,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco2,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco13,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco31,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.closeButton,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco32,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco33,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco3,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco4,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco5,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco6,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco7,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.txt_listado,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco8,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco9,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_referenciado,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco10,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco34,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco35,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco36,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.ctrl_buscar_codigo,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco37,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.cbbox_categoria,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco11,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_codigo,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_categoria,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_modelo,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.ctrl_buscar,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco12,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.ctrl_codigo,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.ctrl_categoria,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.ctrl_modelo,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.ctrl_resultado,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco14,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_stock,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_fecha,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco15,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco16,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco17,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.ctrl_stock,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.ctrl_fecha,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco18,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco19,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco20,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco21,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.modificarButton,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco22,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco23,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco24,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco25,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.refrescarButton,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco26,1,wx.ALIGN_CENTRE_HORIZONTAL),
-        (self.hueco27,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco28,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco29,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.txt_imagen,1,wx.ALIGN_CENTRE_HORIZONTAL),(self.hueco30,1,wx.ALIGN_CENTRE_HORIZONTAL)])
+        self.pnl.SetSizer(self.sizer)
+        self.sizer.Fit(self)
         
-        self.fgs.AddGrowableRow(0, 1) 
-        self.fgs.AddGrowableCol(1, 2)  
-         
-        self.SetSizer(self.fgs)
-        self.Fit()
         
         
 
