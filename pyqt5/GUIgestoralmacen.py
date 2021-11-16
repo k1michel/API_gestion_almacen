2f69df91deaf1bcb5aa0e0c87bc67acb5346e721
Miguel Ángel Álvarez <michel_civic@hotmail.com>
2021-11-16 21:54:37 +0100

    Pruebas sin resultados con PyQt5

--- pyqt5/GUIgestoralmacen.py
+++ pyqt5/GUIgestoralmacen.py
@@ -5,10 +5,10 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 class gui_gestor_almacen(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUI("GUI_Gestor_almacen.ui",self)
+        uic.loadUi("GUI_Gestor_almacen.ui",self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     GUI = gui_gestor_almacen()
-    GUI.Show()
+    GUI.show()
     sys.exit(app.exec_())
