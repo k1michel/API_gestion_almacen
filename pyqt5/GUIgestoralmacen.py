cf631ad241749f8f0d1e8dc8279b8134a7b628d7
Miguel Angel Alvarez <michel_civic@hotmail.com>
2021-11-28 17:40:00 +0100

    Introduzco codigo SOLO para recopilar e insertar en COMBOBOX Categorias.OK.
     Please enter the commit message for your changes. Lines starting

--- pyqt5/GUIgestoralmacen.py
+++ pyqt5/GUIgestoralmacen.py
@@ -11,6 +11,37 @@ class gui_gestor_almacen(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("GUI_Gestor_almacen.ui",self)
+
+        ## RECOPILAR CATEGORIAS DEL INVENTARIO ##
+        recibir_inventario = requests.get('http://localhost:8000/inventario_recibir')
+        list_recibir_inventario = list(recibir_inventario.json())
+        print(list_recibir_inventario)
+        list_dict_inventario = [dict(dict_inventario) for dict_inventario in list_recibir_inventario]
+        print(list_dict_inventario)
+        self.list_categorias = []
+        for i in range(1,len(list_dict_inventario)):    
+            id_item = list_dict_inventario[i]
+            id_item_anterior = list_dict_inventario[i-1]
+            if len(self.list_categorias)>0:
+                if id_item_anterior['categoria'] != id_item['categoria']:
+                    n_list_categorias = 0
+                    for l in range(0,len(self.list_categorias)):
+                        if id_item['categoria'] != self.list_categorias[l]:
+                            n_list_categorias += 1                          
+                    if n_list_categorias == len(self.list_categorias):
+                        self.list_categorias.append(id_item['categoria'])
+            if len(self.list_categorias) == 0:
+                    self.list_categorias.append(id_item['categoria'])    
+        print(f'La lista de categorias es: {self.list_categorias} ')
+        self.list_categorias
+        for c in range(0,len(self.list_categorias)):
+            self.cbbox_categorias.addItem(self.list_categorias[c])
+        #################################################
+        
+        
+                
+        
+        
         self.ctrl_buscar_codigo.editingFinished.connect(self.OnEnterPressedCodigo)
 
     def enviar(self):
@@ -38,6 +69,8 @@ class gui_gestor_almacen(QMainWindow):
         self.busqueda_codigo = self.ctrl_buscar_codigo.text()
         print('Se ha introducido codigo para buscar',self.busqueda_codigo)
         self.enviar()
+
+    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
